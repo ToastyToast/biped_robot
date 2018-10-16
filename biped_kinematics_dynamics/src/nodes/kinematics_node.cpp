23e9bc28f2bd9cccf94974bb2185164e08e774dc
Artem Kovalev <leon112rus@gmail.com>
2018-10-16 18:27:33 +0300

    Add printUntilRoot
    TODO: tests

--- biped_kinematics_dynamics/src/nodes/kinematics_node.cpp
+++ biped_kinematics_dynamics/src/nodes/kinematics_node.cpp
@@ -4,6 +4,8 @@
 
 using namespace biped_kinematics_dynamics;
 
+void printUntilRoot(const RobotLink::Ptr& link);
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "kinematics_node");
@@ -17,6 +19,14 @@ int main(int argc, char** argv)
 
     RobotTree robot_tree(robot_model);
     std::cout << robot_tree << '\n';
+    
+    auto robot_link = robot_tree.findLink("r_ankle_roll_link");
+    printUntilRoot(robot_link);
+    std::cout << '\n';
+    
+    robot_link = robot_tree.findLink("l_ankle_roll_link");
+    printUntilRoot(robot_link);
+    std::cout << '\n';
 
     ROS_INFO("Starting kinematics_node");
     ros::Rate rate(100);
@@ -26,4 +36,27 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+void printUntilRoot(const RobotLink::Ptr& link)
+{
+    auto robot_link = link;
+    while (robot_link) {
+        std::cout << "=== " << robot_link->getLinkName() << " ===\n";
+        auto parent_joint = robot_link->getParentJoint();
+        if (parent_joint) {
+            std::cout << "Parent joint: " << parent_joint->getJointName() << '\n';
+        }
+        
+        auto child_links = robot_link->getChildLinks();
+        if (!child_links.empty()) {
+            std::cout << "( ";
+            for (const auto& child_link : child_links) {
+                std::cout << child_link->getLinkName() << " ";
+            }
+            std::cout << ")\n";
+        }
+        robot_link = robot_link->getParentLink();
+    }
+}
+
 
