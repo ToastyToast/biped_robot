b41b6ffedd4bf7a0e620708d644d2836601652a7
Artem Kovalev <leon112rus@gmail.com>
2019-04-25 19:42:23 +0300

    Test 1D walking

--- biped_control/src/nodes/inverted_pendulum_test_node.cpp
+++ biped_control/src/nodes/inverted_pendulum_test_node.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "inverted_pendulum_test_node");
     ros::NodeHandle nh("");
     
-    ros::Rate rate(20.0f);
+    ros::Rate rate(100.0f);
     
     ros::Publisher x_pos_pub = nh.advertise<std_msgs::Float32>("position", 0);
     ros::Publisher x_vel_pub = nh.advertise<std_msgs::Float32>("velocity", 0);
@@ -22,15 +22,24 @@ int main(int argc, char** argv)
     ROS_INFO("Starting inverted_pendulum_test_node");
     
     float dt = 0.01f;
+    float target_pos = 0.4;
+    float step_size = 0.5f;
     
-    LIP1D lip(0.01f, 0.1f);
+    LIP1D lip(0.01f, 0.25f);
+    lip.setHeight(0.8f);
     
     std_msgs::Float32 msg;
     geometry_msgs::PoseStamped pose_msg;
     while (nh.ok()) {
         lip.integrate(dt);
         
-        std::cout << lip.getPosition() << ", " << lip.getPosition() << "\n";
+        if (lip.getPosition() >= target_pos) {
+            std::cout << "STEP\n";
+            lip.setOrigin(lip.getOrigin() + step_size);
+            target_pos = lip.getOrigin() + step_size/2.0;
+        }
+        
+        std::cout << lip.getPosition() << " -> " << target_pos << " | " << ", " << lip.getVelocity() << "\n";
         
         msg.data = lip.getPosition();
         x_pos_pub.publish(msg);
